00d73782d9171f13d7adba61d6a1f87fc0ab4c3b
airton-silva <airtonsilvan89@gmail.com>
2024-06-04 17:08:27 -0300

    correção no nome do metodo Column escrito errado

--- app/models/tables.py
+++ app/models/tables.py
@@ -23,8 +23,8 @@ class User(db.Model):
 class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Coluns(db.Text)
-    user_id = db.Colums(db.Integer, db.ForeignKey('users.id'))
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     user = db.relationship('User', foreign_keys=user_id)
 
@@ -38,8 +38,8 @@ class Post(db.Model):
 class Follow(db.Model):
     __tablename__ = "follow"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Colums(db.Integer, db.ForeignKey('users.id'))
-    post_id = db.Colums(db.Integer, db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
 
     user = db.relationship('User', foreign_keys=user_id)
     user = db.relationship('Post', foreign_keys=post_id)
