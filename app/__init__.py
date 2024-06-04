42b0967f3d88ed4d913b5daec14bed5a2e3b56f7
airton-silva <airtonsilvan89@gmail.com>
2024-06-03 21:15:00 -0300

    chama variaveis de banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-db = SQLAlchemy(app)
+app.config.from_object('config')
+
+db = SQLAlchemy()
+migrate = Migrate()
+db.init_app(app)
+migrate.init_app(app, db)
+
 
 from app.controllers import default
