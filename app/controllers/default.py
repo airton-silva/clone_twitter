72645f059075466f7e86f7b5324a98db4a5306f1
airton-silva <airtonsilvan89@gmail.com>
2024-06-04 16:03:24 -0300

    criar rota de login e validaçação

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from app import app
 
+from app.models.form import LoginForm
 
-@app.route("/index/<user>")
-@app.route("/", defaults={"user":None})
-def index(user):
-    return render_template('home.html', user=user)
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form) 
