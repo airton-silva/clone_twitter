50cfb58fed3ef271e1493ff750e28a856578421d
airton-silva <airtonsilvan89@gmail.com>
2024-06-05 20:18:55 -0300

    criar rota de login e logou além da rota de listagem de users

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,9 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for
+from app import app, db
 
+from flask_login import login_user, logout_user
+
+from app.models.tables import User
 from app.models.form import LoginForm
 
 @app.route("/index")
@@ -12,8 +15,37 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user.password)
+
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("Logged in.")
+            return redirect(url_for('index'))
+
+
+        else:
+            flash("Invalida login")
+
     else:
         print(form.errors)
     return render_template('login.html', form=form) 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route("/teste/<info>")
+@app.route("/teste", defaults={"info": None})
+def teste(info):
+    usuario = User("airton_silva", "62868642", "Antonio Airton", "aa@mail.com")
+
+    db.session.add(usuario)
+    db.session.commit()
+    return "OK"
+
+@app.route("/users", methods=["GET"])
+def getUsers():
+    read_user = User.query.all()
+    return read_user
