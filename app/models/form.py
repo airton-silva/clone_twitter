ac2f87859fe827cd4f2b6ed5933a10b1831cd669
airton-silva <airtonsilvan89@gmail.com>
2024-06-06 18:56:20 -0300

    criação de class de validação de formulario de criação de usuario

--- app/models/form.py
+++ app/models/form.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,  PasswordField, BooleanField
+from wtforms import StringField,  PasswordField, BooleanField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField("username" , validators=[DataRequired()])
     password =  PasswordField("password", validators=[DataRequired()])
     remember_me = BooleanField("remember_me")
+
+class UserForm(FlaskForm):
+    name = StringField("name" , validators=[DataRequired()])
+    username = StringField("username" , validators=[DataRequired()])
+    password =  PasswordField("password", validators=[DataRequired()])
+    email =  EmailField("email", validators=[DataRequired()])
