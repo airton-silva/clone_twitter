b22942b106daf01c5d48348c49e50821f44f3a8d
airton-silva <airtonsilvan89@gmail.com>
2024-06-08 16:40:25 -0300

    adicionar configurações de uploads

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///twitter.db'
 SECRET_KEY = 'yiirunuci'
+
+UPLOAD_FOLDER = '/img/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
