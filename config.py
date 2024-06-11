d919074119d9841d657e01a20ea7580fb6837e6d
airton-silva <airtonsilvan89@gmail.com>
2024-06-11 18:22:34 -0300

    caminhos de pasta de upload

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///twitter.db'
 SECRET_KEY = 'yiirunuci'
 
-UPLOAD_FOLDER = '/img/uploads'
+UPLOAD_FOLDER = 'app/static/img/uploads'
+BASE_FOLDER_STATIC ='app/static'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
